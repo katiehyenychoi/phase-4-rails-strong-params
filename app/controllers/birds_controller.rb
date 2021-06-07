763a92b3c4af638281121af46f1fe37651da983c
Katie Hyewon Choi <hyewonchoi31@gmail.com>
2021-06-07 15:25:31 -0400

    commit tree ef64a15034e99265aa3ad43b85559a2c778bab82

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -7,8 +7,21 @@ class BirdsController < ApplicationController
   end
 
   # POST /birds
+  # def create
+  #   bird = Bird.create(name: params[:name], species: params[:species])
+  #   render json: bird, status: :created
+  # end
+
+  #Below is forbiddend by Rails built in security
+  # def create
+  #   bird = Bird.create(params)
+  #   render json: bird, status: :created
+  # end
+
+  #Use below instead of above
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    # bird = Bird.create(params.permit(:name, :species))
+    bird = Bird.create(bird_params)
     render json: bird, status: :created
   end
 
@@ -22,4 +35,9 @@ class BirdsController < ApplicationController
     end
   end
 
+  private
+
+  def bird_params
+    params.permit(:name, :species)
+  end
 end
